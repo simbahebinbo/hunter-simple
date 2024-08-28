6252e202035b89732818117a1e55302686fdaa20
simbahebinbo <lansheng228@163.com>
2024-08-28 15:58:19 +0800

    tmp

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/penghuaping/github/my/hunter-simple
+# Build directory: /Users/penghuaping/github/my/hunter-simple/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(SimpleTest "/Users/penghuaping/github/my/hunter-simple/cmake-build-debug/simple")
+set_tests_properties(SimpleTest PROPERTIES  _BACKTRACE_TRIPLES "/Users/penghuaping/github/my/hunter-simple/CMakeLists.txt;41;add_test;/Users/penghuaping/github/my/hunter-simple/CMakeLists.txt;0;")
